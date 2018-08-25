01347156349899c816d414f1926ea9c3018289f1
Kokos14 <42702257+Kokos14@users.noreply.github.com>
2018-08-26 02:00:36 +0300

    v0

--- CrashMiningRobot/CrashMiningRobot/Program.cs
+++ CrashMiningRobot/CrashMiningRobot/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CrashMiningRobot
 {
@@ -10,7 +11,7 @@ namespace CrashMiningRobot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+
         }
     }
 }
--- CrashMiningRobot/CrashMiningRobot/Robot.cs
+++ CrashMiningRobot/CrashMiningRobot/Robot.cs
@@ -6,11 +6,75 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using System.Threading;
 
 namespace CrashMiningRobot
 {
     class Robot
     {
+        IWebDriver driver;
+        string koef;
+        double dkoef;
+        string Hash;
+        //static int access;
+        BET st = new BET();
+        Log RobotLogs = new Log("Robot_Logs");
 
+        public void Start() //запускаем браузер
+        {
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            chromeDriverService.HideCommandPromptWindow = true; //выключаем надоедливую консольку с мусором
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver(chromeDriverService, new ChromeOptions());
+            driver.Navigate().GoToUrl("https://gamdom.com/crash");
+        }
+
+        public void Infinity_cheak() //проверка на конец партии
+        {
+            //access = 0;
+            try { Hash = driver.FindElement(By.XPath("(//INPUT[@type='input'])")).GetAttribute("value").ToString(); 
+
+            do { } while (Hash == driver.FindElement(By.XPath("(//INPUT[@type='input'])")).GetAttribute("value").ToString());
+            }
+            catch { RobotLogs.Add("ERROR: Чтение HASH — " + System.DateTime.Now.ToLongTimeString()); }
+            //access = 1;
+            Console.WriteLine(cheak_win(st.crash));
+            Thread.Sleep(500);
+
+        }
+        public void Bet(int rate, double crash) //ставим
+        {
+            Thread.Sleep(1200);
+            var txtBet = driver.FindElement(By.Name("bet-size"));//находим элемент "кол-во ставки"
+            try
+            {
+                txtBet.Clear();
+                txtBet.SendKeys(Convert.ToString(rate));
+            }
+            catch { }
+            Thread.Sleep(500);
+            var txtBetC = driver.FindElement(By.Name("cash-out"));//находим элемент "коеф на вывод"
+            try
+            {
+                txtBetC.Clear();
+                txtBetC.SendKeys(Convert.ToString(crash));
+            }
+            catch { }
+            Thread.Sleep(1000);
+            try
+            {
+                var BetClick = driver.FindElement(By.XPath("//SPAN[text()='Place Bet']"));//ставим
+                BetClick.Click();
+            }
+            catch { }
+        }
+        public bool cheak_win(double crash) //проверка на победку
+        {
+            try { koef = driver.FindElement(By.XPath("//a[@href='#']/self::a")).Text; } catch { } //парсим последний коэф
+            koef = koef.Remove(koef.Length - 1);
+            koef = koef.Replace('.', ',');
+            dkoef = Double.Parse(koef);
+            if (dkoef >= crash) return true;
+            return false;
+        }
     }
 }
